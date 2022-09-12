255fb32066e79425c7179d1fa6899499561431de
Bass Jansson <tech@bassjansson.com>
2022-09-12 11:59:11 +0200

    Disables auto reconnect and increases DMA buffer size to use around 16K of RAM.

--- trashbox/trashbox.ino
+++ trashbox/trashbox.ino
@@ -34,13 +34,29 @@ void setup()
         // rtc.adjust(DateTime(2022, 9, 10, 20, 48, 0)); // Change this to current time + 40 sec upload time
     }
 
-    // Start bluetooth audio
+    // Setup bluetooth audio
     i2s_pin_config_t i2sPinConfig = {.bck_io_num   = I2S_BCK_PIN,
                                      .ws_io_num    = I2S_LRCK_PIN,
                                      .data_out_num = I2S_DATA_PIN,
                                      .data_in_num  = I2S_PIN_NO_CHANGE};
     a2dpSink.set_pin_config(i2sPinConfig);
-    a2dpSink.start(BL_NAME); // To turn bluetooth on
+
+    static i2s_config_t i2sConfig = {
+        .mode                 = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+        .sample_rate          = 44100, // updated automatically by A2DP
+        .bits_per_sample      = I2S_BITS_PER_SAMPLE_16BIT,
+        .channel_format       = I2S_CHANNEL_FMT_RIGHT_LEFT,
+        .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+        .intr_alloc_flags     = 0,   // default interrupt priority
+        .dma_buf_count        = 16,  // 8 = default
+        .dma_buf_len          = 256, // 64 = default
+        .use_apll             = true,
+        .tx_desc_auto_clear   = true // avoiding noise in case of data unavailability
+    };
+    a2dpSink.set_i2s_config(i2sConfig);
+
+    // Start bluetooth audio
+    a2dpSink.start(BL_NAME, false); // To turn bluetooth on, disable auto reconnect
     // a2dpSink.end(); // To turn bluetooth off
 }
 
